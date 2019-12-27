5adf1e2fc935814eb6f7ee284c360bc0280e39ab
Andrew Copeland <50109276+AndrewCopeland@users.noreply.github.com>
2019-12-27 12:20:50 -0500

    check for env variable

--- program.sh
+++ program.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 main() {
+  if [[ -z "${BOT_APP_API_KEY}" ]]; then
+    echo "ERROR: BOT_APP_API_KEY is not found. Run 'export BOT_APP_API_KEY=<api key>'"
+    exit 1
+  fi
   # get the session token
   curl -s -d "$BOT_APP_API_KEY" -k https://proxy/authn/myConjurAccount/host%2FBotApp%2FmyDemoApp/authenticate > /tmp/conjur_token
   CONT_SESSION_TOKEN=$(cat /tmp/conjur_token| base64 | tr -d '\r\n')
