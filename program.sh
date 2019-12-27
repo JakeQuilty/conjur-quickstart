8568c79f229971cfe352a463e26c41b947f8bb62
Andrew Copeland <50109276+AndrewCopeland@users.noreply.github.com>
2019-12-27 12:18:54 -0500

    update program.sh

--- program.sh
+++ program.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 main() {
+  # get the session token
+  curl -s -d "$BOT_APP_API_KEY" -k https://proxy/authn/myConjurAccount/host%2FBotApp%2FmyDemoApp/authenticate > /tmp/conjur_token
   CONT_SESSION_TOKEN=$(cat /tmp/conjur_token| base64 | tr -d '\r\n')
+  # fetch the secret with the session token obtained from previous step
   VAR_VALUE=$(curl -s -k -H "Content-Type: application/json" -H "Authorization: Token token=\"$CONT_SESSION_TOKEN\"" https://proxy/secrets/myConjurAccount/variable/BotApp%2FsecretVar)
   echo "The retrieved value is: $VAR_VALUE"
 }
